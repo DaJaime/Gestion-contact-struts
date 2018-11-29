fa6512e3285dc36dc035c4ee30cef9ae2f3f2385
Haseeb Chaudhry <haseeb.chaudhry@hotmail.fr>
2018-11-29 09:56:59 +0100

    Ajout du script de la base de donnée

---
+++ scriptDB/db.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS jee;
+CREATE DATABASE jee;
+use jee;
+
+CREATE TABLE Contact (
+ ContactId INT NOT NULL AUTO_INCREMENT,
+ ContactNom VARCHAR(255) NOT NULL,
+ ContactPrenom VARCHAR(255) NOT NULL,
+ ContactAdresse VARCHAR(255) NOT NULL,
+ ContactMail VARCHAR(255) NOT NULL,
+ ContactAutre VARCHAR(255),
+ PRIMARY KEY (ContactId));
+
+CREATE TABLE Telephone (
+ TelephoneId INT NOT NULL AUTO_INCREMENT,
+ TelephoneNumero INT(10) NOT NULL,
+ FKTelephoneContactId INT NOT NULL,
+ DescriptionNumero VARCHAR(255),
+ PRIMARY KEY (TelephoneId));
+
+CREATE TABLE Groupe (
+ GroupeId INT NOT NULL AUTO_INCREMENT,
+ GroupeNom INT(10) NOT NULL,
+ FKGroupeContactId INT NOT NULL,
+ PRIMARY KEY (GroupeId));
+
+ALTER TABLE Telephone
+ADD FOREIGN KEY (FKTelephoneContactId) REFERENCES Contact(ContactId);
+
+ALTER TABLE Groupe
+ADD FOREIGN KEY (FKGroupeContactId) REFERENCES Contact(ContactId);
